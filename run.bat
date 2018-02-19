182ffac959cabe486a4dc894a59d0598246aa05a
l3zA <l3za_wtf@hotmail.com>
2018-02-19 15:08:21 +0700

    Create run.bat

---
+++ run.bat
@@ -0,0 +1,4 @@
+:loop
+%USERPROFILE%\Downloads\ccminer-818-cuda91-x64\ccminer -a neoscrypt -o stratum+tcp://pool1.phi-phi-pool.com:4233 -u 3MJH6zFkwxhGYz2H8j59gYpCcnKAUjsJK7.%COMPUTERNAME% -p c=BTC
+timeout /T 10
+goto loop
