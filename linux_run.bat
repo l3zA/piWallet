5ebd1542408b2c3d4861ed520da87d56ab8e5065
l3zA <l3za_wtf@hotmail.com>
2018-02-24 20:30:46 +0700

    Update linux_run.bat

--- linux_run.bat
+++ linux_run.bat
@@ -1,3 +1,27 @@
+#!/usr/bin/env bash
+
+
+# Load global settings settings.conf
+if ! source ~/settings.conf; then
+        echo "FAILURE: Can not load global settings 'settings.conf'"
+        exit 9
+fi
+
+# Set power limit
+sudo nvidia-smi -pm ENABLED
+sudo nvidia-smi -pl "$MY_WATT"
+echo
+
+export GPU_FORCE_64BIT_PTR=0
+export GPU_MAX_HEAP_SIZE=100
+export GPU_USE_SYNC_OBJECTS=1
+export GPU_MAX_ALLOC_PERCENT=100
+export GPU_SINGLE_ALLOC_PERCENT=100
+
+#
+# ccminer Mining
+#
+
 ~/miner/klaust/ccminer/ccminer -a neoscrypt -o stratum+tcp://pool1.phi-phi-pool.com:4233 -u $MY_ADDRESS.$MY_RIG -p c=BTC
 #~/miner/tpruvot/ccminer/ccminer -a lyra2v2 -o stratum+tcp://pool1.phi-phi-pool.com:4533 -u $MY_ADDRESS.$MY_RIG -p c=BTC
 #~/miner/klaust/ccminer/ccminer -a c11 -o stratum+tcp://pool1.phi-phi-pool.com:3573 -u $MY_ADDRESS.$MY_RIG -p c=BTC
