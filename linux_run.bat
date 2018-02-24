bb6a52f9f87b294d39593d45466ee4185d679231
l3zA <l3za_wtf@hotmail.com>
2018-02-24 20:25:31 +0700

    Create linux_run.bat

---
+++ linux_run.bat
@@ -0,0 +1,3 @@
+~/miner/klaust/ccminer/ccminer -a neoscrypt -o stratum+tcp://pool1.phi-phi-pool.com:4233 -u $MY_ADDRESS.$MY_RIG -p c=BTC
+#~/miner/tpruvot/ccminer/ccminer -a lyra2v2 -o stratum+tcp://pool1.phi-phi-pool.com:4533 -u $MY_ADDRESS.$MY_RIG -p c=BTC
+#~/miner/klaust/ccminer/ccminer -a c11 -o stratum+tcp://pool1.phi-phi-pool.com:3573 -u $MY_ADDRESS.$MY_RIG -p c=BTC
