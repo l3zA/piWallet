80bec6557807a6b25f1fffecc472b2ae2cd8292e
l3zA <l3za_wtf@hotmail.com>
2018-02-27 19:53:38 +0700

    Update linux_run.sh

--- linux_run.sh
+++ linux_run.sh
@@ -22,6 +22,6 @@ export GPU_SINGLE_ALLOC_PERCENT=100
 # ccminer Mining
 #
 
-~/miner/klaust/ccminer/ccminer -a neoscrypt -o stratum+tcp://pool1.phi-phi-pool.com:4233 -u $MY_ADDRESS.$MY_RIG -p c=BTC
+~/miner/klaust/ccminer/ccminer -a neoscrypt -o stratum+tcp://pool1.phi-phi-pool.com:4233 -u $MY_ADDRESS.$MY_RIG -p c=BTC $I_V
 #~/miner/tpruvot/ccminer/ccminer -a lyra2v2 -o stratum+tcp://pool1.phi-phi-pool.com:4533 -u $MY_ADDRESS.$MY_RIG -p c=BTC
 #~/miner/klaust/ccminer/ccminer -a c11 -o stratum+tcp://pool1.phi-phi-pool.com:3573 -u $MY_ADDRESS.$MY_RIG -p c=BTC
