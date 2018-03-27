fb295bd9906940b7269fe70729487021e60d8a6e
l3zA <l3za_wtf@hotmail.com>
2018-03-27 23:42:28 +0700

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,3 +1,4 @@
+call diff.bat
 :loop
 ::%USERPROFILE%\Downloads\ccminer-818-cuda91-x64\ccminer -a neoscrypt -o stratum+tcp://pool1.phi-phi-pool.com:4233 -u 3MJH6zFkwxhGYz2H8j59gYpCcnKAUjsJK7.%COMPUTERNAME% -p c=BTC
 ::%USERPROFILE%\Downloads\ccminer-x64-2.2.4-cuda9\ccminer-x64 -a lyra2v2 -o stratum+tcp://pool1.phi-phi-pool.com:4533 -u 3MJH6zFkwxhGYz2H8j59gYpCcnKAUjsJK7.%COMPUTERNAME% -p c=BTC
@@ -5,7 +6,7 @@
 ::%USERPROFILE%\Downloads\ccminer-xevan\ccminer_x86.exe -a xevan -o stratum+tcp://s.umine.org:3739 -u ScPpvD2vgRB95b8VF2CXdN3RdxG9HpLBQH.%COMPUTERNAME% -p c=XLR
 ::%USERPROFILE%\Downloads\ccminer-x64-2.2.4-cuda9\ccminer-x64 -a nist5 -o stratum+tcp://pool.bsod.pw:3833 -u bNRVpP2b7jrYEw15wZbuuCZgeTNac1WXVD.%COMPUTERNAME% -p c=BWK
 ::%USERPROFILE%\Downloads\ccminer-818-cuda91-x64\ccminer -a neoscrypt  -o stratum+tcp://eu1.fairmine.pro:4250 -u VDtoPYmCL7TMu9Au3sBbxfsYDxeQxRzKqS.%COMPUTERNAME% -d 1,2,3,4 -p c=VIVO
-%USERPROFILE%\Downloads\enemyminer1.03win\ccminer-x64 -a x16r -o stratum+tcp://ravenminer.com:3666 -u RWJ9PDbcpEG9UnC5ehGwK2vYZCxSwXnxxY.%COMPUTERNAME% -p c=RVN
+%USERPROFILE%\Downloads\enemyminer1.03win\ccminer-x64 -a x16r -o stratum+tcp://ravenminer.com:3666 -u RWJ9PDbcpEG9UnC5ehGwK2vYZCxSwXnxxY.%COMPUTERNAME% -p c=RVN%MY_DIFF%
 ::%USERPROFILE%\Downloads\enemyminer1.03win\ccminer-x64 -a x16r -o stratum+tcp://rvn.suprnova.cc:6667 -u l3za.%COMPUTERNAME% -p x
 ::%USERPROFILE%\Downloads\ccminer-818-cuda91-x64\ccminer -a neoscrypt -o stratum+tcp://neoscrypt.mine.ahashpool.com:4233 -u 3MJH6zFkwxhGYz2H8j59gYpCcnKAUjsJK7.%COMPUTERNAME% -p c=BTC
 timeout /T 10
