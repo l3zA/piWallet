b634a7cce296150e5efddd554ece9843c7bcd925
l3zA <l3za_wtf@hotmail.com>
2018-03-29 11:06:43 +0700

    Update run.bat

--- run.bat
+++ run.bat
@@ -11,6 +11,6 @@ call diff.bat
 ::%USERPROFILE%\Downloads\enemyminer1.03win\ccminer-x64 -a x16r -o stratum+tcp://rvn.suprnova.cc:6667 -u l3za.%COMPUTERNAME% -p x
 ::%USERPROFILE%\Downloads\ccminer-818-cuda91-x64\ccminer -a neoscrypt -o stratum+tcp://neoscrypt.mine.ahashpool.com:4233 -u 3MJH6zFkwxhGYz2H8j59gYpCcnKAUjsJK7.%COMPUTERNAME% -p c=BTC
 ::%USERPROFILE%\Downloads\PGNminer2.2.5AddAlgo\ccminer -a x16s -o stratum+tcp://pool1.phi-phi-pool.net:3637 -u PVg5iZzpyEKU4Ts1inkCAXt488CouRCiSM.%COMPUTERNAME% -p c=PGN
-%USERPROFILE%\Downloads\ccminer-x64-2.2.5-rvn-cuda9\ccminer-x64 -a hmq1725 -o stratum+tcp://pool1.phi-phi-pool.net:3747 -u EhVpG3CYGMcuzVMSzJStnBRSCqxGFta17M.%COMPUTERNAME% -p c=ERA
+%USERPROFILE%\Downloads\ccminer-x64-2.2.5-rvn-cuda9\ccminer-x64 -a hmq1725 -o stratum+tcp://hashfaster.com:3748 -u EhVpG3CYGMcuzVMSzJStnBRSCqxGFta17M.%COMPUTERNAME% -p c=ERA
 timeout /T 10
 goto loop
